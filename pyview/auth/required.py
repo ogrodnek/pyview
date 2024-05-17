a6ef15d1a66aade501d4ade49d3639e3c2e0b259
Larry Ogrodnek <ogrodnek@gmail.com>
2024-05-16 22:13:03 -0400

    update fastapi

--- pyview/auth/required.py
+++ pyview/auth/required.py
@@ -4,8 +4,14 @@ from starlette.websockets import WebSocket
 from starlette.authentication import requires as starlette_requires, has_required_scope
 from .provider import AuthProvider, AuthProviderFactory
 from pyview import LiveView
+import sys
 
-_CallableType = typing.TypeVar("_CallableType", bound=typing.Callable)
+if sys.version_info >= (3, 10):  # pragma: no cover
+    from typing import ParamSpec
+else:  # pragma: no cover
+    from typing_extensions import ParamSpec
+
+_P = ParamSpec("_P")
 
 
 @dataclass
@@ -14,7 +20,9 @@ class RequiredScopeAuthProvider(AuthProvider):
     status_code: int = 403
     redirect: typing.Optional[str] = None
 
-    def wrap(self, func: _CallableType) -> _CallableType:
+    def wrap(
+        self, func: typing.Callable[_P, typing.Any]
+    ) -> typing.Callable[_P, typing.Any]:
         return starlette_requires(self.scopes, self.status_code, self.redirect)(func)
 
     def has_required_auth(self, websocket: WebSocket) -> bool:
@@ -28,6 +36,8 @@ def requires(
 ) -> typing.Callable[[type[LiveView]], type[LiveView]]:
     def decorator(cls: type[LiveView]) -> type[LiveView]:
         scopes_list = [scopes] if isinstance(scopes, str) else list(scopes)
-        return AuthProviderFactory.set(cls, RequiredScopeAuthProvider(scopes_list, status_code, redirect))
+        return AuthProviderFactory.set(
+            cls, RequiredScopeAuthProvider(scopes_list, status_code, redirect)
+        )
 
     return decorator
