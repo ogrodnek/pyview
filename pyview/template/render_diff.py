448a39d3462d5ce9941857994913c5c3c643a523
Larry Ogrodnek <ogrodnek@gmail.com>
2024-09-10 22:12:56 -0400

    fix list render diff (#45)

--- pyview/template/render_diff.py
+++ pyview/template/render_diff.py
@@ -11,12 +11,16 @@ def calc_diff(old_tree: dict[str, Any], new_tree: dict[str, Any]) -> dict[str, A
             and "s" in new_tree[key]
             and "d" in new_tree[key]
         ):
+            if isinstance(old_tree[key], str):
+                diff[key] = new_tree[key]
+                continue
+
             # Handle special case of for loop
+            old_static = old_tree[key].get("s", [])
+            new_static = new_tree[key]["s"]
+
             old_dynamic = old_tree[key]["d"]
             new_dynamic = new_tree[key]["d"]
-
-            old_static = old_tree[key]["s"]
-            new_static = new_tree[key]["s"]
 
             if old_static != new_static:
                 diff[key] = {"s": new_static, "d": new_dynamic}
@@ -24,6 +28,7 @@ def calc_diff(old_tree: dict[str, Any], new_tree: dict[str, Any]) -> dict[str, A
 
             if old_dynamic != new_dynamic:
                 diff[key] = {"d": new_dynamic}
+
         elif isinstance(new_tree[key], dict):
             nested_diff = calc_diff(old_tree[key], new_tree[key])
             if nested_diff:
